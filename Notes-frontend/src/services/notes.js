b45a6ff9802724b76fda204bac1aa3c1214d0895
unknown <kumutimam@gmail.com>
2025-04-04 16:14:44 +0200

    Fixed package.json for backend

--- Notes-frontend/src/services/notes.js
+++ Notes-frontend/src/services/notes.js
@@ -1,6 +1,6 @@
 
 import axios from 'axios'
-const baseUrl = '/api/notes'
+const baseUrl = `${import.meta.env.VITE_API_URL}/api/notes`
 
 const getAll = () => {
   const request = axios.get(baseUrl)
